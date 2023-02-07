9c1952b72eb41ea67232b12037933ead0c6df144
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-02-07 12:05:48 -0700

    Add expectations to merchant_request tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -16,23 +16,29 @@ describe 'Merchants API' do
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_a(String)
 
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to eq("merchant")
+      
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
-
+  
   it 'sends a list of one merchant' do
     id = create(:merchant).id
-
+    
     get "/api/v1/merchants/#{id}"
-
+    
     merchant = JSON.parse(response.body, symbolize_names: true)[:data]
-
+    
     expect(response).to be_successful
-
+    
     expect(merchant).to have_key(:id)
     expect(merchant[:id]).to eq("#{id}")
-
+    
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to eq("merchant")
+    
     expect(merchant[:attributes]).to have_key(:name)
     expect(merchant[:attributes][:name]).to be_a(String)
 
