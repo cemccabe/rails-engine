3f65a86ef9a87ae4d9cd113d63a01049ed48f867
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-02-07 23:41:59 -0700

    Add expectations to items_request spec; Pass all tests

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -42,6 +42,24 @@ describe 'Items API' do
 
     expect(response).to be_successful
 
-    item = JSON.parse(response.body, symbolize_names: true)
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to eq("#{id}")
+    
+    expect(item).to have_key(:type)
+    expect(item[:type]).to eq("item")
+    
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+    
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+    
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
 end
