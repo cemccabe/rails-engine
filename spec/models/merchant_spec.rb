eb36c72b54c8774f75a58294e7029ac9fc4916e7
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-02-10 00:14:00 -0700

    Add class method #search tests to merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'class methods' do
+    describe '#search' do
+      it 'finds a merchant by its full name' do
+        create_list(:merchant, 5)
+        merchant = Merchant.first
+
+        expect(Merchant.search(merchant.name)).to eq([merchant])
+      end
+
+      it 'finds a merchant by its partial name' do
+        merchant1 = create(:merchant, name: 'XChristian')
+        merchant2 = create(:merchant, name: 'Chris')
+        merchant3 = create(:merchant, name: 'Sampson')
+
+        expect(Merchant.search('Chr')).to eq([merchant2])
+      end
+    end
+  end
 end
